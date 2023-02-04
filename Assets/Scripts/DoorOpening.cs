c71af1da85f388173fad2e7b3355e26e7694fbd3
DepressedNunu <killian.fravalo@ynov.com>
2023-02-04 03:15:19 +0100

    prefabs

---
+++ Assets/Scripts/DoorOpening.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpening : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
